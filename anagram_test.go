21c4a6473dfca0d530e89ee7613369131efcfa42
mathob Jehanno <mathob.jehanno@hotmail.fr>
2022-01-13 12:45:26 +0100

    Changed function to gather anagram from a list

--- anagram_test.go
+++ anagram_test.go
@@ -1,30 +1,36 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type TestCase struct {
-	Val1, Val2 string
-	Expected   bool
+	Values   []string
+	Expected [][]string
 }
 
 var cases = []TestCase{
 	{
-		"palier",
-		"pareil",
-		true,
-	},
-	{
-		"pareil",
-		"joyeux",
-		false,
+		[]string{
+			"atropine", "tarsiens", "palier", "nitrasse", "tapissera", "plaire", "pointera",
+			"pareil", "parasites", "replia", "repassait", "pianoter", "perlai", "sentiras", "assirent",
+			"passerait", "transies", "astreins",
+		},
+		[][]string{
+			{"atropine", "pointera", "pianoter"},
+			{"tarsiens", "nitrasse", "sentiras", "assirent", "transies", "astreins"},
+			{"tapissera", "parasites", "repassait", "passerait"},
+			{"palier", "plaire", "pareil", "replia", "perlai"},
+		},
 	},
 }
 
 func TestAnagram(t *testing.T) {
 	for _, c := range cases {
-		result := Anagram(c.Val1, c.Val2)
-		if result != c.Expected {
-			t.Errorf("anagram test failed : expected %v, got %v with %s and %s", c.Expected, result, c.Val1, c.Val2)
+		result := Anagrams(c.Values)
+		if !reflect.DeepEqual(result, c.Expected) {
+			t.Errorf("anagram test failed : expected %v, got %v", c.Expected, result)
 		}
 	}
 }
--- main.go
+++ main.go
@@ -3,20 +3,50 @@ package main
 import (
 	"fmt"
 	"os"
-	"reflect"
+	"sort"
 )
 
 func main() {
-	fmt.Println(Anagram(os.Args[1], os.Args[2]))
+	r := Anagrams(os.Args[1:])
+	for _, v := range r {
+		fmt.Println(v)
+	}
 }
 
-func Anagram(word1, word2 string) bool {
-	count1, count2 := map[rune]int{}, map[rune]int{}
-	for _, r := range word1 {
-		count1[r]++
+func Anagrams(input []string) map[string][]string {
+	m := map[string]string{}
+	r := map[string][]string{}
+	for _, refWord := range input {
+		m[refWord] = FindCount(refWord)
 	}
-	for _, r := range word2 {
-		count2[r]++
+
+	for k, v := range m {
+		r[v] = append(r[v], k)
 	}
-	return reflect.DeepEqual(count1, count2)
+
+	return r
+}
+
+func FindCount(word string) string {
+	return SortString(word)
+}
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
 }
